b016e08df7478049dc780e878d85a16785b4324a
perawita <perawitayasa@gmail.com>
2024-08-29 02:32:20 +0800

    try 2

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('module-alias/register');   //medaftarkan module-alias
-require('dotenv').config();
+
 
 const express = require('express');
 const app = express();
@@ -7,12 +7,12 @@ const app = express();
 /**
  * import file dari folder app
  */
-// const env = require('@/config/env.js');
+const env = require('./config/env.js');
 
-const route = require('@/routes/route.js');
-const route_api = require('@/routes/api.js');
+const route = require('./routes/route.js');
+const route_api = require('./routes/api.js');
 
-const origin_middleware = require('@/middlewares/OriginMiddleware.js');
+const origin_middleware = require('./middlewares/OriginMiddleware.js');
 
 /**
  *  Middleware
@@ -27,8 +27,8 @@ app.use('/api', origin_middleware);
 app.use('/', route);
 app.use('/api', route_api);
 
-app.listen(process.env.port, () => console.log
+app.listen(env.port, () => console.log
 (`
-    Server running on port ${process.env.port}
-    - Server: http://localhost:${process.env.port}
+    Server running on port ${env.port}
+    - Server: http://localhost:${env.port}
 `));
