9497cdadff3d345c1dbba8899858f889b2576f12
perawita <perawitayasa@gmail.com>
2024-08-29 00:02:12 +0800

    change

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
-require('module-alias/register');   //medaftarkan module-alias
+// require('module-alias/register');   //medaftarkan module-alias
 
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-/**
- * import file dari folder app
- */
-const env = require('@/config/env.js');
-console.log(`data port di dalam file env.js adalah: ${env.port}`)
+// /**
+//  * import file dari folder app
+//  */
+// const env = require('@/config/env.js');
 
-const route = require('@/routes/route.js');
-const route_api = require('@/routes/api.js');
+// const route = require('@/routes/route.js');
+// const route_api = require('@/routes/api.js');
 
-const origin_middleware = require('@/middlewares/OriginMiddleware.js');
+// const origin_middleware = require('@/middlewares/OriginMiddleware.js');
 
-/**
- *  Middleware
- */
-app.use(express.json());
-app.use('/api', origin_middleware);
+// /**
+//  *  Middleware
+//  */
+// app.use(express.json());
+// app.use('/api', origin_middleware);
 
 
-/**
- *  Routes
- */
-app.use('/', route);
-app.use('/api', route_api);
+// /**
+//  *  Routes
+//  */
+// app.use('/', route);
+// app.use('/api', route_api);
 
-app.listen(env.port, () => console.log
-(`
-    Server running on port ${env.port}
-    - Server: http://localhost:${env.port}
-`));
+// app.listen(env.port, () => console.log
+// (`
+//     Server running on port ${env.port}
+//     - Server: http://localhost:${env.port}
+// `));
+
+console.log(`info masseh: ${env.port}`)
