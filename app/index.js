9145cfea9b01bb3c6a19ee7d8f94fc8d7ce71e62
perawita <perawitayasa@gmail.com>
2024-08-30 02:13:38 +0800

    update

--- app/index.js
+++ app/index.js
@@ -1,73 +1,34 @@
-// Register module-alias first
-require('module-alias/register');
+require('module-alias/register');   //medaftarkan module-alias
+
 
 const express = require('express');
-const fs = require('fs');  // Import fs module
-const path = require('path');  // Import path module for resolving paths
 const app = express();
 
 /**
- * Function to check if a file exists
+ * import file dari folder app
  */
-function checkFileExists(filePath) {
-    return fs.existsSync(filePath);
-}
+const env = require('@/config/env.js');
 
-// Define paths for configuration files
-const envFilePath = path.resolve('app/config/env.js');
-const routeFilePath = path.resolve('app/routes/route.js');
-const routeApiFilePath = path.resolve('app/routes/api.js');
-const originMiddlewarePath = path.resolve('app/middlewares/OriginMiddleware.js');
+const route = require('@/routes/route.js');
+const route_api = require('@/routes/api.js');
 
-// Check if files exist
-if (!checkFileExists(envFilePath)) {
-    console.error(`File not found: ${envFilePath}`);
-    process.exit(1);  // Exit the application if the file doesn't exist
-}
-
-if (!checkFileExists(routeFilePath)) {
-    console.error(`File not found: ${routeFilePath}`);
-    process.exit(1);
-}
-
-if (!checkFileExists(routeApiFilePath)) {
-    console.error(`File not found: ${routeApiFilePath}`);
-    process.exit(1);
-}
-
-if (!checkFileExists(originMiddlewarePath)) {
-    console.error(`File not found: ${originMiddlewarePath}`);
-    process.exit(1);
-}
+const origin_middleware = require('@/middlewares/OriginMiddleware.js');
 
 /**
- * Import file dari folder app
- */
-const env = require(envFilePath);
-const route = require(routeFilePath);
-const route_api = require(routeApiFilePath);
-const origin_middleware = require(originMiddlewarePath);
-
-/**
- * Middleware
+ *  Middleware
  */
 app.use(express.json());
 app.use('/api', origin_middleware);
 
+
 /**
- * Routes
+ *  Routes
  */
 app.use('/', route);
 app.use('/api', route_api);
 
-/**
- * Start server
- */
-app.listen(env.port, () => {
-    console.log(`
+app.listen(env.port, () => console.log
+(`
     Server running on port ${env.port}
     - Server: http://localhost:${env.port}
-  `);
-});
-
-module.exports = app;
+`));
